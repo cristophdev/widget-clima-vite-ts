4bdf4345c71d71bfb8db8b76dc1419907014f1ac
Cristopher Pérez <cristoperesc@gmail.com>
2023-12-11 15:14:03 -0500

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,138 @@
-import { ChangeEventHandler, useState } from 'react';
+import React, { ChangeEventHandler, useState, useCallback, MouseEventHandler } from 'react';
 import './App.css';
 import Day from './assets/day.svg';
 import Thunder from './assets/thunder.svg';
-import Night from './assets/night.svg'
+import Night from './assets/night.svg';
+import d1 from './assets/weather-img/01d.svg';
+import n1 from './assets/weather-img/01n.svg';
+import d2 from './assets/weather-img/02d.svg';
+import n2 from './assets/weather-img/02n.svg';
+import d3 from './assets/weather-img/03d.svg';
+import n3 from './assets/weather-img/03n.svg';
+import d4 from './assets/weather-img/04d.svg';
+import n4 from './assets/weather-img/04n.svg';
+import d9 from './assets/weather-img/09d.svg';
+import n9 from './assets/weather-img/09n.svg';
+import d10 from './assets/weather-img/10d.svg';
+import n10 from './assets/weather-img/10n.svg';
+import d11 from './assets/weather-img/11d.svg';
+import n11 from './assets/weather-img/11n.svg';
+import d13 from './assets/weather-img/13d.svg';
+import n13 from './assets/weather-img/13n.svg';
 
 function App() {
-  const [ country, setCountry ] = useState('');
-  
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [loadData, setLoadData] = useState(false);
+  const [weatherData, setWeatherData] = useState<{
+    weather: string;
+    weatherItIs: string;
+    temp: number;
+    minTemp: number;
+    maxTemp: number;
+    humidity: number;
+  } | "error" | null>(null);
+
+
   const handleCountry: ChangeEventHandler<HTMLInputElement> = (e) => {
     setCountry(e.target.value);
-  }
-  
-  const fetchData = async () => {
+  };
+
+  const fetchData = useCallback(async () => {
     try {
-      const API_KEY = '02210def14bafd3be5c6539498343c8b';
-      const API_LINK = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_KEY}`
-        
-      const resp = await fetch(API_LINK)
-  
+      setLoading(true)
+      const API_KEY: string = '02210def14bafd3be5c6539498343c8b';
+      const API_LINK: string = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_KEY}`;
+
+      const resp = await fetch(API_LINK);
+
       if (!resp.ok) {
-        throw new Error(`Error de red: ${resp.status}`)
+        throw new Error(`Error de red: ${resp.status}`);
       }
-  
+
       const data = await resp.json();
 
-      //weathrr values
-      const temp = data.main.temp - 273.15;
-      const minTemp = data.main.temp_min - 273.15;
-      const maxTemp = data.main.temp_max - 273.15;
-      const humidity = data.main.humidity;
+      // Weather values
+      const weather: string = String(data.weather[0].main);
+      const weatherIcon: string = String(data.weather[0].icon);
+      const temp: number = data.main.temp - 273.15;
+      const minTemp: number = data.main.temp_min - 273.15;
+      const maxTemp: number = data.main.temp_max - 273.15;
+      const humidity: number = data.main.humidity;
 
-      //return the values
-      const weatherData = { temp, minTemp, maxTemp, humidity }
-      return weatherData
-      
+      //
+      const weatherinfo = weatherIcon;
+      const weatherItIs = weatherinfo === "01d" ? d1
+        : weatherinfo === "01n" ? n1
+          : weatherinfo === "02d" ? d2
+            : weatherinfo === "02n" ? n2
+              : weatherinfo === "03d" ? d3
+                : weatherinfo === "03n" ? n3
+                  : weatherinfo === "04d" ? d4
+                    : weatherinfo === "04n" ? n4
+                      : weatherinfo === "09d" ? d9
+                        : weatherinfo === "09n" ? n9
+                          : weatherinfo === "10d" ? d10
+                            : weatherinfo === "10n" ? n10
+                              : weatherinfo === "11d" ? d11
+                                : weatherinfo === "11n" ? n11
+                                  : weatherinfo === "13d" ? d13
+                                    : weatherinfo === "13n" ? n13
+                                      : "";
+
+      // Return the values
+      const weatherData = { weather, weatherItIs, temp, minTemp, maxTemp, humidity };
+      setWeatherData(weatherData);
     } catch (error) {
-      console.error("Error al obtener los datos", error)
+      console.error('Error al obtener los datos', error);
+      setWeatherData("error")
+    } finally {
+      setLoading(false)
     }
+  }, [country]);
+
+  const handleButton: MouseEventHandler = (e) => {
+    e.preventDefault();
+    setLoadData(true);
+    fetchData();
   }
 
-
-  const handleButton = (e: React.FormEvent<HTMLButtonElement>) => { 
-    e.preventDefault()
-    fetchData()
-  }
-  
   return (
     <>
-      <h1 className='titulo'>Clima</h1>
-      <img src={Thunder} className='thunder' />
-      <img src={Night} className='night' />
-      <main className='widget-card'>
+      <h1 className='titulo'>Weather</h1>
+      <img src={Thunder} className='thunder' alt='Thunder' />
+      <img src={Night} className='night' alt='Night' />
+      <main className={`widget-card ${loadData ? 'success' : ''}`}>
         <form className='formulario'>
-          <h3>Escribe tu Ciudad o País</h3>
-          <input id="pais" placeholder='Aquí va tu ciudad o país' onChange={handleCountry} type='text' />
-          <button className='enviar-btn' id='submit' type='submit' onClick={handleButton}>Enviar</button>
-          <p className='nombre'>Hecho por Cristoher Pérez</p>
+          <h3>Enter your City or Country</h3>
+          <input id='pais' placeholder='Here´s your City or Country' onChange={handleCountry} type='text' />
+          <button className='enviar-btn' id='submit' type='submit' onClick={handleButton}>
+            Submit
+          </button>
+          <p className='nombre'>Made by Cristopher Pérez</p>
         </form>
-        <p></p>
+        {loading ?
+          (<p className='cargando'>Loading data...</p>)
+          : weatherData === "error" ?
+            <p className='error'>No results</p>
+            : weatherData ?
+              (<div className='temperatura-resultados'>
+                <div className='clima-info'>
+                  <h2>{weatherData.weather}</h2>
+                  <img src={weatherData.weatherItIs} />
+                </div>
+                <p>Temperature: <span>{weatherData?.temp.toFixed(2)}°</span></p>
+                <p>Temperature Min: <span>{weatherData?.minTemp.toFixed(2)}°</span></p>
+                <p>Temperature Max: <span>{weatherData?.maxTemp.toFixed(2)}°</span></p>
+                <p>Humidity: <span>{weatherData?.humidity}%</span></p>
+              </div>)
+              : null}
       </main>
       <section className='section-img'>
-        <img src={Day} className='sun' />
+        <img src={Day} className='sun' alt='Day' />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
